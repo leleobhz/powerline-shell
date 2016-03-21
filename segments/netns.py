dd162cac8fa2d8a7d126210514720b7028810a74
Leonardo S. Amaral <git@leonardoamaral.com.br>
2016-03-21 14:49:53 -0400

    * Fix netns

--- segments/netns.py
+++ segments/netns.py
@@ -1,11 +1,16 @@
 import subprocess
 
 def add_netns_segment(powerline):
-    env = subprocess.check_output(['ip', 'netns', 'identify', "$$"], stderr=subprocess.STDOUT)
-    if env is None:
+    try:
+        env = subprocess.check_output(['ip', 'netns', 'identify'], stderr=subprocess.STDOUT).rstrip()
+    except Exception, e:    
+        env = str(e.output)
+
+    if not bool(env.strip()):
         return
 
     env_name = os.path.basename(env)
     bg = Color.VIRTUAL_ENV_BG
     fg = Color.VIRTUAL_ENV_FG
-    powerline.append(' %s ' % env_name, fg, bg)
+    powerline.append('NS: %s ' % env_name, fg, bg)
+
