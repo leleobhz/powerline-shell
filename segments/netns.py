dca3687b82debf9241658582802de30cf459d2a6
Leonardo S. Amaral <git@leonardoamaral.com.br>
2016-03-21 14:32:45 -0400

    Initial code for netns

---
+++ segments/netns.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def add_virtual_env_segment(powerline):
+    env = subprocess.check_output(['ip', 'netns', 'identify', "$$"], stderr=subprocess.STDOUT)
+    if env is None:
+        return
+
+    env_name = os.path.basename(env)
+    bg = Color.VIRTUAL_ENV_BG
+    fg = Color.VIRTUAL_ENV_FG
+    powerline.append(' %s ' % env_name, fg, bg)
